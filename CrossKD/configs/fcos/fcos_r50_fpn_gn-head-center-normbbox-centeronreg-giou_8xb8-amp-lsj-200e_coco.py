03cf9106e563fb21adbe07cc303938420e54ee31
Clark <wenjie.hoo@outlook.com>
2025-08-26 22:17:42 +0200

    r18 config

--- CrossKD/configs/fcos/fcos_r50_fpn_gn-head-center-normbbox-centeronreg-giou_8xb8-amp-lsj-200e_coco.py
+++ CrossKD/configs/fcos/fcos_r50_fpn_gn-head-center-normbbox-centeronreg-giou_8xb8-amp-lsj-200e_coco.py
@@ -13,24 +13,28 @@ model = dict(
         bgr_to_rgb=True,
         pad_size_divisor=32,
         batch_augments=batch_augments),
-    backbone=dict(
-        type='ResNet',
-        depth=50,
-        num_stages=4,
-        out_indices=(0, 1, 2, 3),
-        frozen_stages=1,
-        norm_cfg=dict(type='BN', requires_grad=True),
-        norm_eval=True,
-        style='pytorch',
-        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50')),
-    neck=dict(
-        type='FPN',
-        in_channels=[256, 512, 1024, 2048],
-        out_channels=256,
-        start_level=1,
-        add_extra_convs='on_output',  # use P5
-        num_outs=5,
-        relu_before_extra_convs=True),
+backbone=dict(
+    type='ResNet',
+    depth=18,
+    num_stages=4,
+    out_indices=(0, 1, 2, 3),
+    frozen_stages=1,
+    norm_cfg=dict(type='BN', requires_grad=True),
+    norm_eval=True,
+    style='pytorch',  # <- not 'caffe'
+    init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet18')
+),
+
+# --- FPN: channel dims must match R18 C2..C5 ---
+neck=dict(
+    type='FPN',
+    in_channels=[64, 128, 256, 512],  # <- R18 outputs
+    out_channels=256,
+    start_level=0,
+    add_extra_convs='on_output',
+    num_outs=5,
+    relu_before_extra_convs=True
+)
     bbox_head=dict(
         type='FCOSHead',
         num_classes=80,
